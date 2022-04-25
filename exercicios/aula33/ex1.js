ecd89a5978094bcf10ebacdb4fdea1daa49d716d
lpparanhos <paranhos.pan@gmail.com>
2022-04-25 11:56:26 -0300

    2504221156

---
+++ exercicios/aula33/ex1.js
@@ -0,0 +1,12 @@
+var num = 2
+
+//declaração de função
+function soma(num){
+    return num + 3;
+}
+
+//chamada de função e atribuição de valor retornado
+var resultado = soma(num)
+
+//impressão de resultado
+console.log(resultado)
