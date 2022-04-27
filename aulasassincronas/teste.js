673230acc93a09fd9128240f61fd8b5f0daa7c82
lpparanhos <paranhos.pan@gmail.com>
2022-04-27 18:08:58 -0300

    2704221808

---
+++ aulasassincronas/teste.js
@@ -0,0 +1,27 @@
+/* Atalhos relevantes
+ctrl + shift + p > Wrap with Abbreviation == escreve tag de abertura e fechamento em trecho selecionado
+ctrl + ; == comentário de linha
+alt + click == possibilita escrever em múltiplas linhas ao mesmo tempo
+alt + up/down arrow == transporta o código selecionado para cima ou para baixo
+shift + alt + right arrow == seleciona o texto da linha
+shift + alt + up/down arrow == copia o código selecionado para cima ou para baixo
+shift + alt + a == comentário de bloco
+*/
+
+/* 
+    ##Declarando uma função
+    function nomeDaFuncao(parametros) {
+    return parametros
+    }
+    ##Chamando uma função
+    nomeDaFuncao(valoresParaOsParametros);
+    ##Exibindo o valor no console
+    console.log()
+
+    Ex:
+*/
+function soma(a, b){
+    return a + b;
+}
+soma(3, 5); //==8
+console.log(a + b);
