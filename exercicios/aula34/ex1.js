6a48eb276a2c4021a25a37434047290503420295
lpparanhos <paranhos.pan@gmail.com>
2022-04-27 01:48:52 -0300

    2704220148

---
+++ exercicios/aula34/ex1.js
@@ -0,0 +1,6 @@
+const valorDaConta = 101;
+if (valorDaConta < 100){
+    console.log("Saldo suficiente")
+}else{   
+    console.log("Saldo insuficiente")
+}
---
+++ exercicios/aula34/ex2.js
@@ -0,0 +1,14 @@
+var idade;
+idade = 10;
+/* if (idade >=18){
+     console.log("Pode dirigir")
+}else{
+     console.log("Não pode dirigir")
+} */
+if(idade < 12){
+    console.log("Criança")
+}else if(idade >= 12 && idade <18){
+    console.log("Adolescente")
+}else{
+    console.log("Adulto")
+}
