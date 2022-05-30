f473160c5036156ba3f6d5895a66bc38d3f3e8af
lpparanhos <paranhos.pan@gmail.com>
2022-05-30 12:07:05 -0300

    3005221207

---
+++ desafios/formulario-com-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUserForm />
+      <editUserForm />
+    </div>
+  );
+}
+
+export default App;
