e35cb4754cf5fe247baa79fba41077609dfc02bb
lpparanhos <paranhos.pan@gmail.com>
2022-06-27 22:21:59 -0300

    27062222221

---
+++ exercicios/aula73/PrimeiroProjetoJava/src/MeuNome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MeuNome {
+    public static void main(String[] args) {
+
+        Scanner scann = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+
+        String nome = scann.nextLine();
+
+        System.out.print("Salve")
+
+    }
+}
