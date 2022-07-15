fa95315579e2d484324d101a146202024e8e2a4c
lpparanhos <paranhos.pan@gmail.com>
2022-07-15 12:37:01 -0300

    1507221236

---
+++ exercicios/aula92/poo abstração/src/interfaces/CarInterface.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+interface CarInterface {
+
+    // método abstrato - sem corpo
+    public void start();
+
+    public void accelerate();
+
+    public void brake();
+
+}
+
+// public class Car implements CarInterface{
+
+// }
